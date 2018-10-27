644d98ee28dd74baf3460d76dc792a68951545f5
lowemedia <andylowe@low-emedia.com>
2018-10-27 18:05:40 +0100

    Updated configuration and docker build

--- appcode/config/autoload/database.global.php
+++ appcode/config/autoload/database.global.php
@@ -18,7 +18,18 @@ return [
                 'driver_options' => [
                     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
                 ],
+                'hostname' => getenv('DB_ARTICLE_HOST'),
+                'database' => getenv('DB_ARTICLE_SCHEMA'),
+                'username' => getenv('DB_ARTICLE_USER'),
+                'password' => getenv('DB_ARTICLE_PASSWORD'),
             ],
         ],
     ],
+    'elasticsearch' => [
+        'hosts' => [
+            getenv('ELASTICSEARCH_ARTICLE_HOST'),
+            '10.211.55.29:9200'
+//            'vpc-the-hal-project-dkvpkqeip7fitqat3ke5fsrz64.eu-west-1.es.amazonaws.com:80'
+        ],
+    ]
 ];
